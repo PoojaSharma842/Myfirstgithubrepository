956923c737f07fea63fcadfdb250de1311e0fae1
Pooja Sharma <poojasharma@Atuls-MacBook-Air.local>
2020-04-29 18:07:19 +0530

    My first Commit

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,25 @@
+
+public class FirstSeleniumTest{
+	
+public static void convertValues(){
+		
+		byte x;
+		  int a = 270;
+		  double b = 128.128;
+		  System.out.println("int converted to byte");
+		  x = (byte) a;
+		  System.out.println("a and x " + a + " " + x);
+		  System.out.println("double converted to int");
+		  a = (int) b;
+		  System.out.println("b and a " + b + " " + a);
+		  System.out.println("\ndouble converted to byte");
+		  x = (byte)b;
+		  System.out.println("b and x " + b + " " + x);
+		
+	}
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	//Hello
+	convertValues();
+}
+}
